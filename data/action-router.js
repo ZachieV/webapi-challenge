ddc02704eb4f793dee75cbf680fc1ad10f617185
zachary-vincent <45057145+ZachieV@users.noreply.github.com>
2019-08-02 18:59:19 -0700

    get actions

--- data/action-router.js
+++ data/action-router.js
@@ -13,6 +13,18 @@ remove,
  // url begins with /api/actions
 
  // GET ALL ACTIONS =================
+ actionsRouter.get("/", (req, res) => {
+    actions
+      .get()
+      .then(actions => {
+        res.status(200).json(actions);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: "The actions information could not be retrieved." });
+      });
+  });
 // POST  =================
 // PUT  =================
 // DELETE  =================
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //import { listenerCount } from "cluster";
 const listenerCount = require("cluster");
-require('dotenv').config();
+
+require("dotenv").config();
+
 const server = require("./server");
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
-    console.log(`\n*** Amarachi! Server running on http://localhost:${port} ***\n`);
-}); 
+  console.log(
+    `\n*** ZachieV Server running on http://localhost:${port} ***\n`
+  );
+});
